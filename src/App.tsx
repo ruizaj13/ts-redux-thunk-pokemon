d7c89c66bd005011df978b36389868f4152d9890
ruizaj13 <ruizaj13@gmail.com>
2021-06-09 23:24:28 -0700

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import './App.css';
+import { RootStore } from './store';
+import { GetPokemon } from './actions/pokemonActions';
 
 function App() {
+  const [pokemonName, setPokemonName] = useState('')
+  const pokemonState = useSelector((state: RootStore) => state.pokemon)
+  const dispatch = useDispatch()
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPokemonName(event.target.value)
+  }
+
+  const handleClick = () => {
+    dispatch(GetPokemon(pokemonName))
+  }
+
   return (
     <div className="App">
-      Testing
+      <input type='text' onChange={handleChange}/>
+      <button onClick={handleClick}>Search</button>
+      <div>
+        {pokemonState.pokemon && (
+          <div>
+            <img src={pokemonState.pokemon.sprites.front_default} alt=''/>
+            {pokemonState.pokemon.abilities.map( ability => {
+              return <p>{ability.ability.name}</p>
+            })}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
--- src/reducers/PokemonReducer.ts
+++ src/reducers/PokemonReducer.ts
@@ -1,13 +1,32 @@
-interface DefaultStateInterface {
+import { PokemonDispatchTypes, PokemonType, POKEMON_FAIL, POKEMON_LOADING, POKEMON_SUCCESS } from "../actions/pokemonActionTypes";
 
+interface DefaultStateInterface {
+    loading: boolean
+    pokemon?: PokemonType
 }
 
 const defaultState: DefaultStateInterface = {
-
+    loading: false
 };
 
-const pokemonReducer = (state: DefaultStateInterface = defaultState, action: any): DefaultStateInterface => {
-    return state
+const pokemonReducer = (state: DefaultStateInterface = defaultState, action: PokemonDispatchTypes): DefaultStateInterface => {
+    switch (action.type) {
+        case POKEMON_FAIL:
+            return {
+                loading: false
+            }
+        case POKEMON_LOADING: 
+            return {
+                loading: true
+            }
+        case POKEMON_SUCCESS:
+            return {
+                loading: false,
+                pokemon: action.payload
+            }
+        default:
+            return state
+        }
 };
 
 export default pokemonReducer;
